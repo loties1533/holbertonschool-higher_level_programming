fd894aab4f02136e778399630bc5b4186a96a9fb
loties1533 <alexislaubert@yahoo.fr>
2026-01-23 15:46:43 +0000

    fonction pr afficher un carrer de # gere les cas d erreur avec le .txt et docstring

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+module 4-print_square :
+affiche un carré de char #
+"""
+def print_square(size):
+    """
+    affiche un carré de # taille 'size'
+    'size et obligatoirement un entier ou supereir a 0
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for _ in range(size):
+        print("#" * size)
