8a2c418385d4426905c8c1a284173b11dc88393d
loties1533 <alexislaubert@yahoo.fr>
2026-01-23 13:52:03 +0000

    affiche prenom et nom , avec test effectuer docstring et com par fonction

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" module 3-say_my_name
+fonction qui affiche nom et prenom
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    affiche 'my name is (nom,prenom)'
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    if last_name:
+        print(f"My name is {first_name} {last_name}")
+    else:
+        print(f"My name is {first_name}")
