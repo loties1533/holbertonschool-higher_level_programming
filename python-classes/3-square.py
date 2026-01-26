35f08b8c4079df5c3ccb12f4a2d4a8ac79018895
loties1533 <alexislaubert@yahoo.fr>
2026-01-26 21:18:19 +0000

    ajout d une fonction area = calcul et renvoie la surface d un carre

---
+++ python-classes/3-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+classe Square qui definit un carré
+"""
+
+
+class Square:
+    """
+    reprensente un carré avec un attribut privé 'size' pour la taille
+    size vaut 0 par defaut
+    """
+    def __init__(self, size=0):
+        """
+        initialisation de l objet square
+        appelée lors de la création d un nouvel objet
+        size facultatif vaut 0 par dafaut
+        verifie si size est un entier est vaut =< 0
+
+        raise :
+        TypeError si size n est pas en entier
+        ValueError si size  < 0
+
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """
+        calcule et retournel surface du carré
+        size * size
+        """
+        return self.__size * self.__size
