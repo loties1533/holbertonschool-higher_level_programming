9c23e806c77cb23478c99e2e200495b0c3f2f005
loties1533 <alexislaubert@yahoo.fr>
2026-02-03 14:36:17 +0000

    ajout d une class square qui herite de de rectangle et inculion de size dans le validateurs (<0)

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+module square qui va heriter de class rectangle
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """
+    class square qui herite de rectangle
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+
+        return self._Rectangle__width * self._Rectangle__height
