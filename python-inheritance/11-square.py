65eda5d843e0662ce7b2f470fea1d9293410359e
loties1533 <alexislaubert@yahoo.fr>
2026-02-04 10:23:46 +0000

    ajout du 'size' en priver plus doc a jour

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -12,13 +12,13 @@ class Square(Rectangle):
     def __init__(self, size):
         self.integer_validator("size", size)
         super().__init__(size, size)
+        self.__size = size
 
     def area(self):
-
-        return self._Rectangle__width * self._Rectangle__height
+        return self.__size * self.__size
 
     def __str__(self):
         """
         retourne la représentation en string du carré
         """
-        return "[Square] {}/{}".format(self.__width, self.__height)
+        return "[Square] {}/{}".format(self.__size, self.__size)
