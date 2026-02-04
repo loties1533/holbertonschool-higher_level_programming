959881f408af2531daed788f2555374c24cef35c
loties1533 <alexislaubert@yahoo.fr>
2026-02-04 09:52:35 +0000

    doctring valide

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 """
-fonction qui verifie si l objet est une instence de class ou de sous class
+Module 3-is_kind_of_class
+contient une fonction qui vérifie si un objet est une instance d'une classe
+ou d'une sous-classe
 """
 
 
 def is_kind_of_class(obj, a_class):
     """
-    retourn 'true' si si 'obj' est une instence de 'a_class' ou sous class
-    sinon retourn 'false'
+    Vérifie si un objet est une instance d'une classe demander
+    ou d'une sous-classe
 
-    args
-    - obj = l objet verifier
-    - a_class = la class a comparer 
-    (si c bien une instence de cette class ou sous class)
+    Args:
+        obj (objet): L'objet à tester
+        a_class (type): La classe à vérifier
 
-    return
-        - true si obj et bien une instence de a_class ou une sous class ,
-        sinon false
+    Returns:
+        bool: True si obj est une instance de a_class ou d'une sous-classe,
+        sinon False
     """
     return isinstance(obj, a_class)
