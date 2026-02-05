5352a268dc58b278c7d08cb8214a7c38176996b0
loties1533 <alexislaubert@yahoo.fr>
2026-02-05 14:31:35 +0000

    rempplacement de insistance

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -22,7 +22,7 @@ class BaseGeometry:
         leve TypeError si ce n'est pas un entier ou ValueError si <= 0
         """
 
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
