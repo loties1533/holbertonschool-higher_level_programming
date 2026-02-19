60d2440de37505575d56bb45dd7a80233849b9df
loties1533 <alexislaubert@yahoo.fr>
2026-02-19 09:06:58 +0000

    Création d'un serveur HTTP simple avec do_GET gérant plusieurs endpoints

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+class APIHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+
+        elif self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode())
+
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK ")
+
+        elif self.path == "/info":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            info = {"version": "1.0","description": "a simple API built with http.server"}
+            self.wfile.write(json.dumps(info).encode())
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"endpoint not found")
+
+
+PORT = 8000
+httpd = HTTPServer(("", PORT), APIHandler)
+print(f"Serving on port {PORT}...")
+httpd.serve_forever()
