48921af841e70ccf5836291280f7d3cf921658e0
loties1533 <alexislaubert@yahoo.fr>
2026-03-03 14:44:03 +0000

    ajout de la ','

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- creéation d'une table avec l'ID et name
 
-CREATE TABLES IF NOT EXISTS first_table (
-    id INT
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
     name VARCHAR(256)
-)
+);
