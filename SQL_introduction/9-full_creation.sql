e6dfa205f0761f0707039b92e53bef6508f8d760
loties1533 <alexislaubert@yahoo.fr>
2026-03-03 18:08:02 +0000

    creation de second_table ,et insertion de plusieurs lignes

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- scipt pour créé une nouvelle tables dans la meme bdd (hbtn_0c_0)
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
