c665e5918c84dc40040cb75b76a424f80b71950a
loties <alexislaubert@yahoo.fr>
2026-03-04 11:50:49 +0100

    Script pour lister les privilèges de user_0d_1 et user_0d_2

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- script qui répertorie tous les privilèges des utilisateurs MySQL user_0d_1 et user_0d_2
+-- serveur localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
