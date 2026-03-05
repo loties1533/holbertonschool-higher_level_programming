e0dcf81639e436849cabe9d548b8a47c2facb467
loties <alexislaubert@yahoo.fr>
2026-03-05 16:30:05 +0100

    correction du ';'

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- script qui liste les séries uniquement sans genre
+-- affiche titre et l'ID de la catégorie/genre triés par titre et genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
