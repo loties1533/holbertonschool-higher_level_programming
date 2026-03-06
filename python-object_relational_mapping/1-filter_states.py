70532168792b0da1dd678c6ba6772f13ba7fab28
loties <alexislaubert@yahoo.fr>
2026-03-06 11:28:20 +0100

    liste tt les etats qui commence pas N

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+script qui liste tout les states (états) de la base de données hbtn_0e_0_usa qui commencent par la lettre N
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+    cursor.execute("""SELECT *
+                FROM states
+                WHERE name LIKE 'N%'
+                ORDER BY id ASC""")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
