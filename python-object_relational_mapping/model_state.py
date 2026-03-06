9fb8c0fe845d946bff2a98af58e444f8ec167354
loties <alexislaubert@yahoo.fr>
2026-03-06 20:40:32 +0100

    definition classe State et instance Base declarative_base, mapping SQLAlchemy vers table states

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+definition classe State liée a table States (SQLAlchemy)
+Base = instance de declarative_base()
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """
+    classe State liée a table States
+    """
+    __tablename__ = 'states'
+
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
