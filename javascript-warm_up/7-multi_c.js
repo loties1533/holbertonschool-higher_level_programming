7d01db4fb885627fecd322e8613df800ce186a4c
loties <alexislaubert@yahoo.fr>
2026-03-20 21:10:02 +0100

    affiche un msg x fois et verifie erreur d arg

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const numTimes = parseInt(process.argv[2]);
+if (isNaN(numTimes)) {
+  console.log('Missing number of occurrences');
+}
+else {
+  for (let count = 0; count < numTimes; count++) {
+    console.log('C is fun');
+  }
+}
