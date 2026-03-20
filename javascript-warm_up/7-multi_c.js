fa6cabe4a021b026a4fdf877f9b2a08d98fca835
loties <alexislaubert@yahoo.fr>
2026-03-20 21:24:16 +0100

    chmod le fichier

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -3,8 +3,7 @@
 const numTimes = parseInt(process.argv[2]);
 if (isNaN(numTimes)) {
   console.log('Missing number of occurrences');
-}
-else {
+} else {
   for (let count = 0; count < numTimes; count++) {
     console.log('C is fun');
   }
