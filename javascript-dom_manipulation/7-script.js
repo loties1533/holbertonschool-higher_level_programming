ea8fda6045a7d9bedab1b67268e852734acac367
loties <alexislaubert@yahoo.fr>
2026-03-23 16:16:13 +0100

    script qui recupere et affiche tout les titres de film depuis l api et les affiche sous forme de liste 'ul'

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,15 @@
+const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
+const listMoviesUl = document.getElementById('list_movies');
+
+fetch(url)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    data.results.forEach(function (movie) {
+      const listItem = document.createElement('li');
+      listItem.textContent = movie.title;
+      listMoviesUl.appendChild(listItem);
+    });
+  });
+  
